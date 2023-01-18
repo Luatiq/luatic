ea9d297993884de8960d60cd662aacaa758a20d5
Lua <47286632+Luatiq@users.noreply.github.com>
2023-01-18 15:56:01 +0100

    added ciel60 page

---
+++ pages/keyboards/ciel60.vue
@@ -0,0 +1,93 @@
+<script setup></script>
+
+<template>
+  <keyboard-slot :keeb="keeb">
+    <template #title>
+      <h1 class="top-heading">
+        The daily driver.
+      </h1>
+    </template>
+
+    <template #about>
+      <section>
+        <p>
+          I bought the Ciel60 kit because I wanted to try an O-ring mounted keyboard with a weight, I was not prepared
+          for just how good it would be.
+        </p>
+
+        <h2 class="top-heading">
+          Component selection
+        </h2>
+        <p>
+          I did not put much thought into the keyboard kit (case, pcb, plate, o-ring) itself. Though
+          obviously I did have some requirements:
+        </p>
+
+        <ul>
+          <li>
+            A cute color. this kit had a
+            <span style="background: #FCB0FF; color: black; padding: 0 .25rem">lilac</span>
+            option, perfect!
+          </li>
+          <li>
+            A somewhat flexible plate, but not too flexible. By default it came with steel, but I wanted FR4.
+            Why settle?
+          </li>
+          <li>
+            A soft mount. This kit always mounts via O-ring, but by default it's a 50A o-ring. How are my poor fingers
+            gonna type on that? I needed the softer 30A o-ring.
+          </li>
+        </ul>
+
+        <h4 class="not-a-heading">
+          That's it for the kit, it's Switch&trade; time.
+        </h4>
+        <p>
+          I needed linear switches that were not too light. I also wanted to have a box stem for as little stem wobble
+          as possible. Also, I like the sound of a POM stem.
+        </p>
+        <p>
+          While this does not narrow it down as much as I would have liked, it did give me something to work with.
+          Luckily these switches are pink! Just my favorite wavelength of light, yay!
+        </p>
+
+        <h4 class="not-a-heading">
+          Now for the keycaps
+        </h4>
+        <p>
+          I've run with multiple sets for this keeb. And my by-far favourite is a simple black-on-white set, I went with
+          MW CMM BOW. How clean!
+        </p>
+
+        <h4 class="not-a-heading">
+          We do a little bit of foaming
+        </h4>
+        <p>
+          I wanted to go for the popular "marbly" sound, but without muting <b>everything</b>. So I went with foam for
+          in between the plate & pcb. The case does also have foam though! Just not an entire mat like most keyboards
+          have. I kept it simple with a thin strip of EVA foam.
+        </p>
+      </section>
+    </template>
+  </keyboard-slot>
+</template>
+
+<!-- <style>
+    @import '@/assets/home.css';
+</style> -->
+
+<script>
+import ciel60 from './specs/ciel60.json'
+import KeyboardSlot from '@/slots/KeyboardSlot'
+
+export default {
+  components: {
+    KeyboardSlot
+  },
+  data () {
+    return {
+      keeb: ciel60
+    }
+  }
+}
+</script>
---
+++ slots/KeyboardSlot.vue
@@ -0,0 +1,252 @@
+<template>
+  <div class="container keeb-main">
+    <main>
+      <slot name="title">
+        <h1>
+          Keebs
+        </h1>
+      </slot>
+
+      <slot name="about">
+        <section>
+          About keeb!
+        </section>
+      </slot>
+    </main>
+
+    <aside class="card keeb-specs-card">
+      <table>
+        <thead>
+          <tr style="display: none">
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('case', $event)">
+            <td>Case</td>
+            <td>{{ keeb.case.display }}</td>
+            <td :class="detailsVisible.case ? 'caseChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.case" class="specs-detailed">
+            <tr>
+              <td>Material</td>
+              <td>{{ keeb.case.material }}</td>
+            </tr>
+            <tr>
+              <td>Dampening</td>
+              <td>{{ keeb.case.dampening }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('pcb', $event)">
+            <td>PCB</td>
+            <td>{{ keeb.pcb.display }}</td>
+            <td :class="detailsVisible.pcb ? 'pcbChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.pcb" class="specs-detailed">
+            <tr>
+              <td>Dampening</td>
+              <td>{{ keeb.pcb.dampening ? keeb.pcb.dampening : 'None' }}</td>
+            </tr>
+            <tr>
+              <td>Mounting style</td>
+              <td>{{ keeb.pcb.mounting_style }}</td>
+            </tr>
+            <tr>
+              <td>Flex cuts</td>
+              <td>{{ keeb.pcb.flex_cuts ? 'Yes' : 'No' }}</td>
+            </tr>
+            <tr>
+              <td>Thickness</td>
+              <td>{{ keeb.pcb.thickness + 'mm' }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('plate', $event)">
+            <td>Plate</td>
+            <td>{{ keeb.plate.material }}</td>
+            <td :class="detailsVisible.plate ? 'plateChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.plate" class="specs-detailed">
+            <tr>
+              <td>Dampening</td>
+              <td>{{ keeb.plate.dampening }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('switches', $event)">
+            <td>Switches</td>
+            <td>{{ keeb.switches.display }}</td>
+            <td :class="detailsVisible.switches ? 'switchesChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.switches" class="specs-detailed">
+            <tr>
+              <td>Type</td>
+              <td>{{ keeb.switches.type }}</td>
+            </tr>
+            <tr>
+              <td>Top housing material</td>
+              <td>{{ keeb.switches.top_housing }}</td>
+            </tr>
+            <tr>
+              <td>Bottom housing material</td>
+              <td>{{ keeb.switches.bottom_housing }}</td>
+            </tr>
+            <tr>
+              <td>Stem material</td>
+              <td>{{ keeb.switches.stem }}</td>
+            </tr>
+            <tr>
+              <td>Number of pins</td>
+              <td>{{ keeb.switches.nr_of_pins }}</td>
+            </tr>
+            <tr>
+              <td>Total travel</td>
+              <td>{{ keeb.switches.total_travel + 'mm' }}</td>
+            </tr>
+            <tr>
+              <td>Actuation travel</td>
+              <td>{{ keeb.switches.actuation_travel + 'mm' }}</td>
+            </tr>
+            <tr>
+              <td>Actuation force</td>
+              <td>{{ keeb.switches.actuation_force + 'g' }}</td>
+            </tr>
+            <tr>
+              <td>Bottom-out force</td>
+              <td>{{ keeb.switches.bottom_out_force + 'g' }}</td>
+            </tr>
+            <tr>
+              <td>Lubed with</td>
+              <td>{{ keeb.switches.lube }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('keycaps', $event)">
+            <td>Keycaps</td>
+            <td>{{ keeb.keycaps.display }}</td>
+            <td :class="detailsVisible.keycaps ? 'keycapsChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.keycaps" class="specs-detailed">
+            <tr>
+              <td>Profile</td>
+              <td>{{ keeb.keycaps.profile }}</td>
+            </tr>
+            <tr>
+              <td>Material</td>
+              <td>{{ keeb.keycaps.material }}</td>
+            </tr>
+            <tr>
+              <td>Legend method</td>
+              <td>{{ keeb.keycaps.legend_method }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('stabs', $event)">
+            <td>Stabilizers</td>
+            <td>{{ keeb.stabs.display }}</td>
+            <td :class="detailsVisible.stabs ? 'stabsChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.stabs" class="specs-detailed">
+            <tr>
+              <td>Mounting type</td>
+              <td>{{ keeb.stabs.mounting_type }}</td>
+            </tr>
+            <tr>
+              <td>Stem material</td>
+              <td>{{ keeb.stabs.stem }}</td>
+            </tr>
+            <tr>
+              <td>Housing material</td>
+              <td>{{ keeb.stabs.housing }}</td>
+            </tr>
+            <tr>
+              <td>Lubed with</td>
+              <td>{{ keeb.stabs.lube }}</td>
+            </tr>
+          </tbody>
+        </transition>
+        <tbody>
+          <tr class="keebSpecsClickable" title="Toggle info" @click="toggleDetails('suppLayouts', $event)">
+            <td>Supported layouts</td>
+            <td>{{ keeb.supported_layouts.length }}</td>
+            <td :class="detailsVisible.suppLayouts ? 'suppLayoutsChevronOpen' : ''">
+              <ChevronRightIcon />
+            </td>
+          </tr>
+        </tbody>
+        <transition name="accordion">
+          <tbody v-if="detailsVisible.suppLayouts" class="specs-detailed">
+            <tr v-for="(layout) in keeb.supported_layouts" :key="layout">
+              <td class="select-none">&nbsp;</td>
+              <td>
+                {{ layout }}
+              </td>
+            </tr>
+          </tbody>
+        </transition>
+      </table>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KeyboardSlot',
+  props: {
+    keeb: Object
+  },
+  data () {
+    return {
+      detailsVisible: {
+        case: false,
+        pcb: false,
+        plate: false,
+        switches: false,
+        keycaps: false,
+        stabs: false,
+        suppLayouts: false
+      }
+    }
+  },
+  methods: {
+    toggleDetails (arg) {
+      this.detailsVisible[arg] = !this.detailsVisible[arg]
+    }
+  }
+}
+</script>
+
+<style>
+@import '@/assets/styles/keyboards.css';
+</style>
